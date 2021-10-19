f1449d1b2b4b1def6c07080255ce0f499bb5f053
charludo <github@charlotteharludo.com>
2021-10-19 11:43:23 +0200

    wip: auto-generating new layers

--- src/rulesets/thermometer.py
+++ src/rulesets/thermometer.py
@@ -17,3 +17,44 @@ class Thermometer(Ruleset):
 
         possible = [p for p in permutations(ks, len(steps)) if p == tuple(sorted(p))]
         return grouped(or_clause([grouped(and_clause([f"{steps[i]}{p[i]}" for i in range(len(p))])) for p in possible]))
+
+
+def generate_new(max=4):
+    from random import randint, shuffle
+
+    lines = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    shuffle(lines)
+    for line in lines[0:randint(0, max)]:
+        rule = create_new()
+
+        before_pad = [0 for i in range(randint(0, 9 - len(rule)))]
+        after_pad = [0 for i in range(9 - len(rule) - len(before_pad))]
+        rule = before_pad + rule + after_pad
+
+        if randint(0, 1) == 0:
+            yield to_row(rule, line)
+        else:
+            yield to_col(rule, line)
+
+
+def to_row(rule, row):
+    str_row = "".join([str(x) if x != 0 else "." for x in rule])
+    return "........." * row + str_row + "........." * (8 - row)
+
+
+def to_col(rule, col):
+    str_col = "".join([str(x) if x != 0 else "." for x in rule])
+    return "".join(["." * col + str_col[i] + "." * (8 - col) for i in range(9)])
+
+
+def create_new():
+    from random import randint
+
+    thermo = list(range(1, randint(3, 6)))
+    if randint(0, 1) == 1:
+        thermo.reverse()
+
+    return thermo
+
+
+print("\n".join("".join([str(i) for i in x]) for x in generate_new()))
